c2ab47b3c8b2d02b1630b379cb68eee2b11422c1
Youssef <yelabd@purdue.edu>
2017-03-19 23:38:25 -0400

    Required done

---
+++ InstagramClone/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  InstagramClone
+//
+//  Created by Youssef Elabd on 3/19/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
---
+++ InstagramClone/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  InstagramClone
+//
+//  Created by Youssef Elabd on 3/19/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
